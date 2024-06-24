e77136f2f3442f4974b1513573b485e4a48c14da
brajesh708 <brajeshmewada694@gmail.com>
2024-06-24 12:37:37 +0530

    new preatic

--- prectci.py
+++ prectci.py
@@ -5,13 +5,13 @@
 
 # print=(range(0,10))
 
-# x=range(-1,-11,-1)
-# print(list(x))
+x=range(-1,-11,-1)
+print(list(x))
 
 
 
-# x=range(1,11,-1)
-# print(list(x))
+x=range(1,11,-1)
+print(list(x))
 
 
 # x=range(5)
