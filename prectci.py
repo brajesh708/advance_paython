d38cd471bfb5e4e9951732a1fb63d5abe78923b3
brajesh708 <brajeshmewada694@gmail.com>
2024-07-01 12:31:48 +0530

    new data

--- prectci.py
+++ prectci.py
@@ -1,6 +1,5 @@
 # 
-# x=range(0,10)
-# print(x)
+#
 
 
 # print=(range(0,10))
@@ -10,8 +9,8 @@ print(list(x))
 
 
 
-x=range(1,11,-1)
-print(list(x))
+# x=range(1,11,-1)
+# print(list(x))
 
 
 # x=range(5)
