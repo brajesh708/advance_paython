c641f634695e92522f11f9335be754a7a67eb3f0
brajesh708 <brajeshmewada694@gmail.com>
2024-07-08 17:30:36 +0530

    new

---
+++ reducer.py
@@ -0,0 +1,14 @@
+import functools
+my_list=[10,20,40,34,45,50]
+
+
+
+from functools import reduce
+my_list=[10,20,30,40,50,43,23]
+def max(x,y):
+    if x>y:
+        return x
+    else:
+        return y
+x= reduce(max,my_list)
+print(x)
