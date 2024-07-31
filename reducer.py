0734b992f301bc4e62dd651f65044826011333bf
brajesh708 <brajeshmewada694@gmail.com>
2024-07-31 13:34:28 +0530

    reduce

--- reducer.py
+++ reducer.py
@@ -12,3 +12,14 @@ def max(x,y):
         return y
 x= reduce(max,my_list)
 print(x)
+
+
+from functools import reduce
+my_list=[10,20,30,40,50,43,23]
+def min(x,y):
+    if x<y:
+        return x
+    else:
+        return y
+mini=reduce(min,my_list)
+print(mini)
