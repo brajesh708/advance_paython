c46546811dce61a079d3e17d7db371632257f559
brajesh708 <brajeshmewada694@gmail.com>
2024-08-09 22:50:15 +0530

    new

--- opps/Method.py
+++ opps/Method.py
@@ -1,37 +1,37 @@
-class Student:
-    def display(self,name):
-        self.name=name
-        print("name=",name)
+# class Student:
+#     def display(self,name):
+#         self.name=name
+#         print("name=",name)
         
-    def show(self,age):
-        self.age=age
-        self.display("raj")
-        print("age=",age)
-obj=Student()
-obj.display("Brajesh")
-obj.show(22)
+#     def show(self,age):
+#         self.age=age
+#         self.display("raj")
+#         print("age=",age)
+# obj=Student()
+# obj.display("Brajesh")
+# obj.show(22)
 
 
 #  class methode banane ki 
 # @classmethod
 # self  ke place per use cls
 
-class Book:
-    price=1000
-    def book_detail(self,name,author):
-        self.name=name
-        self.author=author
-    @classmethod
-    def update_price(cls,price):
-        cls.price=price
-    def show_data(self):
-        print('Book Name=',self.name)
-        print('Book Author=',self.author)
-        print('Book Price=',self.price)
-obj=Book()
-obj.book_detail('Python','Vaishu')
-obj.update_price(1500)
-obj.show_data()
+# class Book:
+#     price=1000
+#     def book_detail(self,name,author):
+#         self.name=name
+#         self.author=author
+#     @classmethod
+#     def update_price(cls,price):
+#         cls.price=price
+#     def show_data(self):
+#         print('Book Name=',self.name)
+#         print('Book Author=',self.author)
+#         print('Book Price=',self.price)
+# obj=Book()
+# obj.book_detail('Python','Vaishu')
+# obj.update_price(1500)
+# obj.show_data()
 
     # static methode 
 # class Student:
